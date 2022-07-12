0fc454bd716a2dff4cc06e4aee92edd73d66986b
SeoYangHun <syh8088@docfriends.com>
2022-07-12 22:36:51 +0900

    Update Person.java

--- part1/effective-java-part1/src/main/java/me/whiteship/chapter01/item03/methodreference/Person.java
+++ part1/effective-java-part1/src/main/java/me/whiteship/chapter01/item03/methodreference/Person.java
@@ -1,9 +1,12 @@
 package me.whiteship.chapter01.item03.methodreference;
 
+import autovalue.shaded.com.google$.common.base.$Function;
+
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.function.Supplier;
 
 public class Person {
 
@@ -22,6 +25,9 @@ public class Person {
     }
 
     public static void main(String[] args) {
+
+        Supplier<Person> localDatePerson$Functio2n = Person::new;
+        $Function<LocalDate, Person> localDatePerson$Function = Person::new;
         List<Person> people = new ArrayList<>();
         people.add(new Person(LocalDate.of(1982, 7, 15)));
         people.add(new Person(LocalDate.of(2011, 3, 2)));
@@ -33,6 +39,8 @@ public class Person {
                 return a.birthday.compareTo(b.birthday);
             }
         });
+
+        people.sort(Person::compareByAge);
     }
 
     public int getAge() {
