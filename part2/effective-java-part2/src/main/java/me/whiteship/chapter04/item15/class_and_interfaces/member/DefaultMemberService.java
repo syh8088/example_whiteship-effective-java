bbdaee11188fbfb01fe17d9e5fbba7b6f7a1aee1
SeoYangHun <syh8088@docfriends.com>
2022-10-21 23:02:10 +0900

    Update DefaultMemberService.java

--- part2/effective-java-part2/src/main/java/me/whiteship/chapter04/item15/class_and_interfaces/member/DefaultMemberService.java
+++ part2/effective-java-part2/src/main/java/me/whiteship/chapter04/item15/class_and_interfaces/member/DefaultMemberService.java
@@ -6,12 +6,20 @@ class DefaultMemberService implements MemberService {
 
     private String name;
 
+    /**
+     * 한 클래스에서만 사용하는 package-private 클래스나 인터페이스는 해당 클래
+     * 스에 private static으로 중첩 시키자. (아이템 24)
+     */
     private static class PrivateStaticClass {
-
+        void doPrint() {
+            //System.out.println("name = " + name);
+        }
     }
 
     private class PrivateClass {
-
+        void doPrint() {
+            System.out.println("name = " + name);
+        }
     }
 
     public static void main(String[] args) {
