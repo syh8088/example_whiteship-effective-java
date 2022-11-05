da67a3a4989e12ea750231a073b53e5cc2dc4c76
SeoYangHun <syh8088@docfriends.com>
2022-11-05 15:21:14 +0900

    주석 설명 추가
    주석 설명 추가

--- part2/effective-java-part2/src/main/java/me/whiteship/chapter05/item26/genericdao/GenericRepository.java
+++ part2/effective-java-part2/src/main/java/me/whiteship/chapter05/item26/genericdao/GenericRepository.java
@@ -4,6 +4,10 @@ import java.util.HashSet;
 import java.util.Optional;
 import java.util.Set;
 
+/**
+ * 한정된 타입 메개변수
+ * @param <E>
+ */
 public class GenericRepository<E extends Entity> {
 
     private Set<E> entities;
--- part2/effective-java-part2/src/main/java/me/whiteship/chapter05/item26/terms/Box.java
+++ part2/effective-java-part2/src/main/java/me/whiteship/chapter05/item26/terms/Box.java
@@ -1,6 +1,10 @@
 package me.whiteship.chapter05.item26.terms;
 
-public class Box<E> {
+// public class Box<E> {
+// public class Box<E extends Entity> { // E extends Entity <-- 한정된 타입 메개변수
+// public class Box<?> { // ? <-- 비 한정적 와이드 카드 타입
+
+public class Box<E> {  // E <-- 타입 매게변수
 
     private E item;
 
