382f0650f6eb50c221c05a5caa01e9ad5b27b5ab
SeoYangHun <syh8088@docfriends.com>
2022-11-06 22:43:22 +0900

    Retention Target 설명 추가
    Retention Target 설명 추가

--- part2/effective-java-part2/src/main/java/me/whiteship/chapter05/item27/annotation/MyAnnotation.java
+++ part2/effective-java-part2/src/main/java/me/whiteship/chapter05/item27/annotation/MyAnnotation.java
@@ -2,6 +2,13 @@ package me.whiteship.chapter05.item27.annotation;
 
 import java.lang.annotation.*;
 
+/**
+ * @Retention: 애노테이션의 정보를 얼마나 오래 유지할 것인가.
+ * • Runtime, Source, Class
+ *
+ * @Target: 애노테이션을 사용할 수 있는 위치.
+ * • Type, Field, Method, Parameter,
+ */
 @Retention(RetentionPolicy.SOURCE)
 @Target({ElementType.TYPE, ElementType.METHOD})
 public @interface MyAnnotation {
